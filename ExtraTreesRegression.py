952c42631da281f153ce5cce5d3d1d57c6b142cb
TheInfoLight <asddinar999@mail.ru>
2019-04-21 22:53:37 +0300

    Extra Trees Regression model extracted and saved, attempt to write interface

--- ExtraTreesRegression.py
+++ ExtraTreesRegression.py
@@ -3,6 +3,7 @@ import numpy as np
 from sklearn.ensemble import ExtraTreesRegressor
 from sklearn.metrics import mean_squared_error, r2_score, mean_absolute_error
 from sklearn.model_selection import train_test_split
+import pickle
 
 x, y = datahelper.get_xy('data/', num_hours=3, error_minutes=15)
 
@@ -21,25 +22,32 @@ for n in estimators:
 
 sorted_by_scores = [(k, scores[k]) for k in sorted(scores, key=scores.get, reverse=True)]
 
-print('Results of 5 estimators giving best results:\n')
+index, score = sorted_by_scores[0]
+extra_trees.set_params(n_estimators=estimators[index])
+extra_trees.fit(x_train, y_train)
 
-for i in range(0, 5):
-    index, score = sorted_by_scores[i]
-    print("Number of estimators = ", estimators[index])
+filename = 'models/extra_trees_model.sav'
+print('Saving model with ', estimators[index], ' estimators to file ', filename)
+pickle.dump(extra_trees, open(filename, 'wb'))
 
-    extra_trees.set_params(n_estimators=estimators[index])
-    extra_trees.fit(x_train, y_train)
-    y_predicted = extra_trees.predict(x_test)
 
-    print('R^2 = ' + str(r2_score(y_test, y_predicted)))
-    print('MSE = ' + str(np.sqrt(mean_squared_error(y_test, y_predicted))))
-    print()
-
-    m = mean_absolute_error(y_test, y_predicted, multioutput='raw_values')
-    print('Average mean absolute error: ', np.average(m))
-    print("Mean absolute error for measurements:")
-    for col, err in zip(list(x_test.columns.values), m):
-        print(col, ": ", err)
-    print()
+# for i in range(0, 5):
+#     index, score = sorted_by_scores[i]
+#     print("Number of estimators = ", estimators[index])
+#
+#     extra_trees.set_params(n_estimators=estimators[index])
+#     extra_trees.fit(x_train, y_train)
+#     y_predicted = extra_trees.predict(x_test)
+#
+#     print('R^2 = ' + str(r2_score(y_test, y_predicted)))
+#     print('MSE = ' + str(np.sqrt(mean_squared_error(y_test, y_predicted))))
+#     print()
+#
+#     m = mean_absolute_error(y_test, y_predicted, multioutput='raw_values')
+#     print('Average mean absolute error: ', np.average(m))
+#     print("Mean absolute error for measurements:")
+#     for col, err in zip(list(x_test.columns.values), m):
+#         print(col, ": ", err)
+#     print()
 
 
---
+++ Interface.py
@@ -0,0 +1,44 @@
+import pickle
+from datetime import datetime, timedelta
+import pandas as pd
+import numpy as np
+import json
+
+
+def predict_conditions(model_path, time, wind_speed, wind_gusts, dew_point_temperature, road_temperature,
+                       underground_temperature, air_temperature, snow_intensity, rain_intensity, humidity):
+    # Convert arguments to dataframe needed for model:
+    columns = ['wind_speed', 'wind_gusts', 'dew_point_temperature', 'road_temperature', 'underground_temperature',
+               'air_temperature',
+               'snow_intensity', 'rain_intensity', 'humidity']
+    x = np.array(columns)
+    row = [wind_speed, wind_gusts, dew_point_temperature, road_temperature, underground_temperature, air_temperature,
+           snow_intensity,
+           rain_intensity, humidity]
+    x = np.vstack([x, row])
+    dataframe = pd.DataFrame(columns=x[0], data=x[1:])
+
+    # Add three hours to initial time:
+    time_end = datetime.strptime(time, "%Y-%m-%d %H:%M UTC")
+    time_end = time_end + timedelta(hours=3)
+
+    # Load saved model:
+    loaded_model = pickle.load(open(model_path, 'rb'))
+
+    # Predict values:
+    predicted = loaded_model.predict(dataframe)
+
+    # Create dictionary in roadcast format:
+    roadcast_dict = dict()
+    roadcast_dict['timestamp'] = str(time_end)
+    roadcast_dict['wind_speed'] = predicted[0][0]
+    roadcast_dict['wind_gusts'] = predicted[0][1]
+    roadcast_dict['dew_point_temperature'] = predicted[0][2]
+    roadcast_dict['road_temperature'] = predicted[0][3]
+    roadcast_dict['underground_temperature'] = predicted[0][4]
+    roadcast_dict['air_temperature'] = predicted[0][5]
+    roadcast_dict['snow_intensity'] = predicted[0][6]
+    roadcast_dict['rain_intensity'] = predicted[0][7]
+    roadcast_dict['humidity'] = predicted[0][8]
+
+    return json.dumps(roadcast_dict)
